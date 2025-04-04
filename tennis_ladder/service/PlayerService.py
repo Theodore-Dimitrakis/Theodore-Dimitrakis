5e362ac1dd0e43b84f6f9d9ae52e3201b9d9a85e
AlexPeti <alexandros.peteinaris@hotmail.com>
2025-04-04 21:00:32 +0300

    Renamed player dao to player repository

--- tennis_ladder/service/PlayerService.py
+++ tennis_ladder/service/PlayerService.py
@@ -1,6 +1,6 @@
 class PlayerService:
-    def __init__(self, player_dao):
-        self.player_dao = player_dao
+    def __init__(self, player_repository):
+        self.player_repository = player_repository
 
     def add_player(self, name: str, rank: int):
-        return self.player_dao.create(name, rank)
+        return self.player_repository.create(name, rank)
