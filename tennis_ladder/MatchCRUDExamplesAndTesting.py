a2a2dec93766be7016e115db5112ae92e3a31492
AlexPeti <alexandros.peteinaris@hotmail.com>
2025-05-17 15:34:43 +0300

    Refactored the print result of the new show_ordered_by_rank methods and tested it

--- tennis_ladder/MatchCRUDExamplesAndTesting.py
+++ tennis_ladder/MatchCRUDExamplesAndTesting.py
@@ -3,7 +3,7 @@ from repository.LeagueRoundRepository import LeagueRoundRepository
 from repository.MatchRepository import MatchRepository
 from repository.PlayerRepository import PlayerRepository
 from service.MatchService import MatchService
-
+from service.PlayerService import PlayerService
 
 # Initialize DB session using existing DBInit logic
 db_init = DBInit()
@@ -16,6 +16,7 @@ player_dao = PlayerRepository(session)
 match_dao = MatchRepository(session)
 league_round_repository = LeagueRoundRepository(session)
 match_service = MatchService(session, match_dao, player_dao, league_round_repository)
+player_service = PlayerService(session, player_dao)
 
 def test_create_match(challenger_id, opponent_id):
     print(f"\nCreating match between player {challenger_id} and player {opponent_id}:")
@@ -28,7 +29,11 @@ if __name__ == "__main__":
 
 
     #match_service.create_match(4,1)
-    match_service.simulate_round(4, 3)
+
+    #match_service.simulate_round(4, 3)
+    #player_service.show_statistics()
+    player_service.show_ordered_by_rank()
+
 
     # Test listing all matches after creation
     #test_list_all_matches()
--- tennis_ladder/service/PlayerService.py
+++ tennis_ladder/service/PlayerService.py
@@ -49,10 +49,12 @@ class PlayerService:
     def show_statistics(self ):
         players = self.player_repository.get_all()
         for player in players:
-            print(f"Name: {player.name}, Rank: {player.rank}, Wins: {player.wins}, Losses: {player.losses}")
+            print(f"Name: {player.name}, Rank: {player.rank}, Wins: {player.total_wins}, Losses: {player.total_losses}")
 
     def show_ordered_by_rank(self) -> list[Player]:
         players = self.player_repository.get_all()
-        list_ladder = sorted(players, key=lambda p: p.rank, reverse=True)
-        return [(p.name, p.rank) for p in  list_ladder ]
+        list_ladder = sorted(players, key=lambda p: p.rank)
+        for p in list_ladder:
+            print(f"{p.rank}. {p.name}")
+        return list_ladder
 
