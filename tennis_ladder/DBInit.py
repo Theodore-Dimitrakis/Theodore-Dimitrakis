5ac5cf862d343f78abc62b69a7d076e2674890e2
AlexPeti <alexandros.peteinaris@hotmail.com>
2025-05-31 16:41:24 +0300

    Added new checks in initialize db that will prohibit the end user from running the database initialization if the tournament has begun by looking at the existence of the tennis_ladder.db file. Added a new method for restarting the database that deletes the tennis_ladder.db file, no matter if the tournament is in progress or not. Changed the match service methods to return the results of the games along with the names of the winner and loser for every round. When the games of the 20th league round have been played, instead of returning the games, we return the tournament winner (ranked 1 player after 20 rounds of games have been played)

--- tennis_ladder/DBInit.py
+++ tennis_ladder/DBInit.py
@@ -9,13 +9,21 @@ from entity.LeagueRound import LeagueRound
 class DBInit:
     def __init__(self):
         project_root = os.path.dirname(os.path.abspath(__file__))
-        db_path = os.path.join(project_root, 'tennis_ladder.db')
-        self.db_url = f"sqlite:///{db_path}"
+        self.db_path = os.path.join(project_root, 'tennis_ladder.db')
+        self.db_url = f"sqlite:///{self.db_path}"
         self.engine, self.session = DBUtils.create_engine_and_session(self.db_url)
+
+    def database_exists(self):
+        return os.path.exists(self.db_path)
 
     def create_db(self):
         DBUtils.create_tables(self.engine)
         print("Database and tables created successfully")
+
+    def delete_database(self):
+        if self.database_exists():
+            os.remove(self.db_path)
+            print("Existing database deleted.")
 
     def insert_initial_players(self):
         players = [
@@ -32,10 +40,18 @@ class DBInit:
         self.session.commit()
 
     def initialize_db(self):
+        if self.database_exists():
+            raise RuntimeError("Database already exists. Initialization aborted.")
         self.create_db()
         self.insert_initial_players()
 
+    def restart_db(self):
+        self.delete_database()
+        self.__init__()
+        self.initialize_db()
+
 if __name__ == "__main__":
     db_init = DBInit()
-    db_init.initialize_db()
+    #db_init.initialize_db()
+    db_init.restart_db()
 
